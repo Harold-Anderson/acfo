792ba628c54d4484f5ec7224426e137e4a14195d
Harold Anderson <5002519+Harold-Anderson@users.noreply.github.com>
2024-11-01 06:19:26 -0700

    added @playform/inline to inline css

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,8 @@ import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehype
 import { fileURLToPath } from 'url';
 import path from 'path';
 
+import playformInline from '@playform/inline';
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const hasExternalScripts = false;
 const whenExternalScripts = (items = []) => hasExternalScripts ? Array.isArray(items) ? items.map(item => item()) : [items()] : [];
@@ -20,49 +22,45 @@ export default defineConfig({
   site: 'https://www.acfo.co',
   integrations: [
     tailwind({
-      applyBaseStyles: false
-    }),
-    sitemap(),
-    mdx(),
-    icon({
-      include: {
-        tabler: ['*'],
-        'flat-color-icons': [
-          'binoculars',
-          'voice-presentation',
-          'combo-chart',
-          'business-contact',
-          'process',
-          'document',
-          'template'
-        ],
-        'mdi': ['strategy'],
-        'streamline': ['subscription-cashflow'],
-        'fluent-emoji': ['magnifying-glass-tilted-right'],
-        'material-symbols': ['communication'],
+    applyBaseStyles: false
+  }), 
+  sitemap(), 
+  mdx(), 
+  icon({
+    include: {
+      tabler: ['*'],
+      'flat-color-icons': [
+        'binoculars',
+        'voice-presentation',
+        'combo-chart',
+        'business-contact',
+        'process',
+        'document',
+        'template'
+      ],
+      'mdi': ['strategy'],
+      'streamline': ['subscription-cashflow'],
+      'fluent-emoji': ['magnifying-glass-tilted-right'],
+      'material-symbols': ['communication'],
+    }
+  }), ...whenExternalScripts(() => partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  })), compress({
+    CSS: true,
+    HTML: {
+      'html-minifier-terser': {
+        removeAttributeQuotes: false
       }
-    }),
-    ...whenExternalScripts(() => partytown({
-      config: {
-        forward: ['dataLayer.push']
-      }
-    })),
-    compress({
-      CSS: true,
-      HTML: {
-        'html-minifier-terser': {
-          removeAttributeQuotes: false
-        }
-      },
-      Image: false,
-      JavaScript: true,
-      SVG: false,
-      Logger: 1
-    }),
-    astrowind({
-      config: './src/config.yaml'
-    })
-  ],
+    },
+    Image: false,
+    JavaScript: true,
+    SVG: false,
+    Logger: 1
+  }), astrowind({
+    config: './src/config.yaml'
+  }), playformInline()],
   markdown: {
     remarkPlugins: [readingTimeRemarkPlugin],
     rehypePlugins: [responsiveTablesRehypePlugin, lazyImagesRehypePlugin]
